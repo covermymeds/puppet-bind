86df8a5467931ea8121ceb0d422beafffb5aec19
Steven Anthony <santhony@covermymeds.com>
2021-06-18 14:15:36 -0400

    regex

--- manifests/ptr_cidr_zone.pp
+++ manifests/ptr_cidr_zone.pp
@@ -28,13 +28,13 @@ define bind::ptr_cidr_zone (
 
   # Select the invalid cidr_ptr_zones and warn about them.
   # Refer to RFC-1034
-  $_invalid_cidr_ptr_zone = $cidr_ptr_zone.filter |$keys, $values| { $keys !~ /[a-zA-Z0-9.\-]*/ }
+  $_invalid_cidr_ptr_zone = $cidr_ptr_zone.filter |$keys, $values| { $keys !~ /^[a-zA-Z0-9.\-]*$/ }
   $_invalid_cidr_ptr_zone.each |$key, $value| {
     warning( "bind_validation_failure\: The hostname for \'${key}\' at \'${value}\' is not valid.")
   }
 
   # Select only the valid cidr_ptr_zones so that we don't break the zonefile template
-  $_valid_cidr_ptr_zone = $cidr_ptr_zone.filter |$keys, $values| { $keys =~ /[a-zA-Z0-9.\-]*/ }
+  $_valid_cidr_ptr_zone = $cidr_ptr_zone.filter |$keys, $values| { $keys =~ /^[a-zA-Z0-9.\-]*$/ }
 
   file{ "/var/named/zone_${cidr_ptr}":
     ensure  => present,
